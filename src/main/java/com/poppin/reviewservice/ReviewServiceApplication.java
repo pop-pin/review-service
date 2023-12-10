cc20377b29060a40ffac84910520fa3b5d226900
codrin2 <hssang127@gmail.com>
2023-12-10 22:15:55 +0900

    build: eureka client setting

--- src/main/java/com/poppin/reviewservice/ReviewServiceApplication.java
+++ src/main/java/com/poppin/reviewservice/ReviewServiceApplication.java
@@ -2,7 +2,9 @@ package com.poppin.reviewservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ReviewServiceApplication {
 
--- src/main/java/com/poppin/reviewservice/controller/ReviewController.java
+++ src/main/java/com/poppin/reviewservice/controller/ReviewController.java
@@ -13,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/review")
 @RequiredArgsConstructor
 public class ReviewController {
 
